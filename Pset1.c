41f77f119b2df11615a9c844e9e3a725b2234b90
nowherelang <nowherelang@users.noreply.github.com>
2019-05-27 18:42:06 +0800

    Update Pset1.c

--- Pset1.c
+++ Pset1.c
@@ -1,1 +1,21 @@
+#######################
+##这里是problem set 1##
+#######################
+
+# 包含三个问题
+## 第一个 ##
+## Implement Hello
+## 第二个 ##
+## Implement either of:
+    Mario, less comfortable
+    Mario, more comfortable
+## 第一个 ##
+## Implement either of:
+    Cash, less comfortable
+    Credit, more comfortable
+
+#########笔记###############################
+help50 make hello 可以查找bug是什么原因
+style50 hello.c 可以帮助评价code有什么缺点
+
 
